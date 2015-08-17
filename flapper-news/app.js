285399549c62992cae4e4d01bc0ba694d528e130
Vity <lensherr@gmail.com>
2015-08-17 14:39:39 +0200

    añadido passport

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -9,8 +9,13 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
 
 mongoose.connect('mongodb://localhost/news');
+
+//autenticacion passport
+var passport = require('passport');
+require('./config/passport.js');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -31,6 +36,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+app.use(passport.initialize());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -135,5 +135,21 @@ router.put('/posts/:post/comments/:comment/downvote', function(req, res, next) {
   });
 });
 
+//registro de usuario
+router.posts('/register', function(req, res, next) {
+  if (!req.body.username || !req.body.password){
+    return res.status(400).json({message: "Please fill our all the fields"});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+  user.setPassword(req.body.password);
+
+  user.save(function (err) {
+    if (err){return next(err);}
+    return res.json({token: user.generateJWT();})
+  });
+});
 
 module.exports = router;
